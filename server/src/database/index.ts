c4b07b76d9db9882ad5c0beb99890a472df54987
Rizal Sofyan <work.rizalsofyans@gmail.com>
2022-07-05 22:35:39 +0700

    Add connection to mongodb-atlas

---
+++ server/src/database/index.ts
@@ -0,0 +1,13 @@
+import { MongoClient } from 'mongodb';
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`;
+
+export const connectDatabase = async () => {
+	const client = await MongoClient.connect(url);
+
+	const db = client.db('main');
+
+	return {
+		listings: db.collection('test_renthouse'),
+	};
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,22 @@
-import express from 'express';
+require('dotenv').config();
+
+import express, { Application } from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { typeDefs, resolvers } from './graphql';
+import { connectDatabase } from './database';
 
-const app = express();
-const port = 8080;
+const mount = async (app: Application) => {
+	const db = await connectDatabase();
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		context: () => ({ db }),
+	});
+	server.applyMiddleware({ app, path: '/api' });
 
-const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app, path: '/api' });
+	app.listen(process.env.PORT);
 
-app.listen(port);
+	console.log(`[App]: http://localhost:${process.env.PORT}`);
+};
 
-console.log(`[App]: http://localhost:${port}`);
+mount(express());
